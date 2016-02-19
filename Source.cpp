4c205a1e2182251f638700341bdc3b482f82a116
rakielsoomaroo <soomaroorakiel@ymail.com>
2016-02-19 11:25:14 +0200

    master

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <= 20; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				cout << "FizzBuzzWoof" << endl;
+			}
+			else{
+				cout << "Fizz" << endl;
+			}
+		}
+		else
+		{
+			if (i % 5 == 0)
+			{
+				cout << "Buzz" << endl;
+			}
+			else
+			{
+				cout << i << endl;
+			}
+		}
+
+	}
+
+	system("pause");
+}
