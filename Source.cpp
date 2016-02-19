da0b0aaf4f561294ea46c12a0951931226c28181
rakielsoomaroo <soomaroorakiel@ymail.com>
2016-02-19 11:45:13 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -10,47 +10,47 @@ int main()
 		{
 
 			cout << "FizzBuzzWoof" << endl;
-		}
+		}//first if
 		else
 		{
 			if (i % 3 == 0 && i % 5 == 0)
 			{
 				cout << "FizzBuzz" << endl;
-			}
+			}//second if
 
 			else
 			{
 				if (i % 3 == 0 && i % 7 == 0)
 				{
 					cout << "FizzWoof" << endl;
-				}
+				}//third if
 				else
 				{
 					if (i % 5 == 0 && i % 7 == 0)
 					{
 						cout << "BuzzWoof" << endl;
-					}
+					}//fourth if
 					else if (i % 3 == 0)
 					{
 						cout << "Fizz" << endl;
-					}
+					}//fifth if
 					else if (i % 5 == 0)
 					{
 						cout << "Buzz" << endl;
-					}
+					}//sixth if
 					else if (i % 7 == 0)
 					{
 						cout << "Woof" << endl;
-					}
+					}//seventh if
 					else
 					{
 						cout << i << endl;
-					}
-				}
-			}
-		}
+					}//seventh else
+				}//third else
+			}//second else
+		}//first else
 
 
 
-	}
-}
+	}//for loop
+}//int main
