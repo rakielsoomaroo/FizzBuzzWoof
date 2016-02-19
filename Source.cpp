9a70fb028ced1e767c89270a6545d34bc6f63db5
rakielsoomaroo <soomaroorakiel@ymail.com>
2016-02-19 11:38:25 +0200

    woof

--- Source.cpp
+++ Source.cpp
@@ -4,31 +4,53 @@ using namespace std;
 
 int main()
 {
-	for (int i = 1; i <= 20; i++)
+	for (int i = 1; i <= 50; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
 		{
-			if (i % 5 == 0)
-			{
-				cout << "FizzBuzzWoof" << endl;
-			}
-			else{
-				cout << "Fizz" << endl;
-			}
+
+			cout << "FizzBuzzWoof" << endl;
 		}
 		else
 		{
-			if (i % 5 == 0)
+			if (i % 3 == 0 && i % 5 == 0)
 			{
-				cout << "Buzz" << endl;
+				cout << "FizzBuzz" << endl;
 			}
+
 			else
 			{
-				cout << i << endl;
+				if (i % 3 == 0 && i % 7 == 0)
+				{
+					cout << "FizzWoof" << endl;
+				}
+				else
+				{
+					if (i % 5 == 0 && i % 7 == 0)
+					{
+						cout << "BuzzWoof" << endl;
+					}
+					else if (i % 3 == 0)
+					{
+						cout << "Fizz" << endl;
+					}
+					else if (i % 5 == 0)
+					{
+						cout << "Buzz" << endl;
+					}
+					else if (i % 7 == 0)
+					{
+						cout << "Woof" << endl;
+					}
+					else
+					{
+						cout << i << endl;
+					}
+				}
 			}
 		}
 
-	}
 
-	system("pause");
+
+	}
 }
